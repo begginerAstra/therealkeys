f624c6a777bcd9ba442018310554af054423f22a
begginerAstra <euandersonguedes@gmail.com>
2025-05-17 18:38:04 -0300

    Create keys.lua

---
+++ keys.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        Identificador = "Astra",
+        Hwid = "3E90CA7A-D9FE-4725-819D-002521F34CA1",
+        Key = "SUPA-A7X92N3KD"
+    },
+    {
+        Identificador = "Matheus",
+        Hwid = "C354810B-A7AB-444A-9AFF-169183A9230",
+        Key = "SUPA-AFSAFSAF7SAF75A"
+    }
+}
