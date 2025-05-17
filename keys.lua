f6015c395c5f070f3aa45bcf84417269ac12a197
begginerAstra <euandersonguedes@gmail.com>
2025-05-17 18:47:33 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -8,5 +8,15 @@ return {
         Identificador = "Matheus",
         Hwid = "C354810B-A7AB-444A-9AFF-169183A9230",
         Key = "SUPA-AFSAFSAF7SAF75A"
+    },
+    {
+        Identificador = "Demon",
+        Hwid = "BF974E3D-8DB6-48F6-90B2-7EF5FFA09639",
+        Key = "SUPA-F0s9MCns952398NSlsafç9NSm"
+    },
+    {
+        Identificador = "Alt",
+        Hwid = "CB9A94F5-E30C-4358-89AB-43D2A99689DA",
+        Key = "SUPA-fm0SncS8kfasnCSCSA8wdfsa"
     }
 }
