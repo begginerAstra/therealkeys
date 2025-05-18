433619df7091331ef938979ef50e7a5223331346
begginerAstra <euandersonguedes@gmail.com>
2025-05-18 02:28:13 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -6,7 +6,7 @@ return {
     },
     {
         Identificador = "Matheus",
-        Hwid = "C354810B-A7AB-44A-9AFF-169183A59230",
+        Hwid = "C354810B-A7AB-44AA-9AFF-169183A59230",
         Key = "SUPA-oEfn08snfF99CNsYFsS"
     },
     {
