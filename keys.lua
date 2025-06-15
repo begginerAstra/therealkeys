cb3115c05cd2a37ec6a04d24261f46a8017e10e3
begginerAstra <euandersonguedes@gmail.com>
2025-06-15 12:30:37 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,41 +1,41 @@
-return {
-    keys = {
+{
+    "keys": [
         {
-            key = "SUPA-BPEHGTC5BHC4I6QDF4E37V2N1GN5QL",
-            created_at = "2025-06-15 15:11:34",
-            expires_at = "2025-07-15 15:11:34",
-            used = false,
-            used_by = nil,
-            used_at = nil
+            "key": "SUPA-BPEHGTC5BHC4I6QDF4E37V2N1GN5QL",
+            "created_at": "2025-06-15 15:11:34",
+            "expires_at": "2025-07-15 15:11:34",
+            "used": false,
+            "used_by": null,
+            "used_at": null
         },
         {
-            key = "SUPA-UO62SKBBU6WJDKQ0TL945O5ZDOVYE5",
-            created_at = "2025-06-15 15:19:26",
-            expires_at = "2025-06-16 15:19:26",
-            used = false,
-            used_by = nil,
-            used_at = nil
+            "key": "SUPA-UO62SKBBU6WJDKQ0TL945O5ZDOVYE5",
+            "created_at": "2025-06-15 15:19:26",
+            "expires_at": "2025-06-16 15:19:26",
+            "used": false,
+            "used_by": null,
+            "used_at": null
         }
-    },
-    premium_users = {},
-    logs = {
+    ],
+    "premium_users": [],
+    "logs": [
         {
-            action = "CHAVE_GERADA",
-            user = "244hzkj",
-            timestamp = "2025-06-15 15:11:37",
-            details = "Chave: SUPA-BPEHGTC5BHC4I6QDF4E37V2N1GN5QL, Duração: 30 dias"
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 15:11:37",
+            "details": "Chave: SUPA-BPEHGTC5BHC4I6QDF4E37V2N1GN5QL, Duração: 30 dias"
         },
         {
-            action = "SINCRONIZADO_GITHUB",
-            user = "244hzkj",
-            timestamp = "2025-06-15 15:19:11",
-            details = "Sincronização manual"
+            "action": "SINCRONIZADO_GITHUB",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 15:19:11",
+            "details": "Sincronização manual"
         },
         {
-            action = "CHAVE_GERADA",
-            user = "244hzkj",
-            timestamp = "2025-06-15 15:19:28",
-            details = "Chave: SUPA-UO62SKBBU6WJDKQ0TL945O5ZDOVYE5, Duração: 1 dias"
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 15:19:28",
+            "details": "Chave: SUPA-UO62SKBBU6WJDKQ0TL945O5ZDOVYE5, Duração: 1 dias"
         }
-    }
+    ]
 }
