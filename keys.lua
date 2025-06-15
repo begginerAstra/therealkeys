efad2417050da0174706ec66c32f85816cf1c73e
begginerAstra <euandersonguedes@gmail.com>
2025-06-15 12:19:11 -0300

    Atualizar keys.lua

--- keys.lua
+++ keys.lua
@@ -1,77 +1,21 @@
-return {
-    {
-        Identificador = "Astra",
-        Hwid = "FBF70412-A7C7-4A93-9FB9-CB6E2913D167",
-        Key = "SUPA-Aiaiaaibolsonaro"
-    },
-    {
-        Identificador = "Matheus (offline)",
-        Hwid = "offC354810B-A7AB-44AA-9AFF-169183A59230",
-        Key = "SUPA-93215928125215251"
-    },
-    {
-        Identificador = "Demon",
-        Hwid = "D3C7C611-8639-4CBC-BFFD-224489160D03",
-        Key = "SUPA-F0s9MCns952398NSlsafç9NSm"
-    },
-    {
-        Identificador = "Alt",
-        Hwid = "6698AC15-1C75-43FC-A637-0345B7DA8572",
-        Key = "SUPA-fm0SncS8kfasnCSCSA8wdfsa"
-    },
-    {
-        Identificador = "Kaizel",
-        Hwid = "",
-        Key = "SUPA-FS0mNS8524sfnS01423sC25412"
-    },
-    {
-        Identificador = "gsrou (Perma key)",
-        Hwid = "B1A9C92B-C5D8-4E21-A65D-73B875749CB6",
-        Key = "SUPA-942mFso0sncWS1s42s"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    },
-    {
-        Identificador = "(vacant)",
-        Hwid = "(vacant)",
-        Key = "(vacant)"
-    }
+{
+    "keys": [
+        {
+            "key": "SUPA-BPEHGTC5BHC4I6QDF4E37V2N1GN5QL",
+            "created_at": "2025-06-15 15:11:34",
+            "expires_at": "2025-07-15 15:11:34",
+            "used": false,
+            "used_by": null,
+            "used_at": null
+        }
+    ],
+    "premium_users": [],
+    "logs": [
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 15:11:37",
+            "details": "Chave: SUPA-BPEHGTC5BHC4I6QDF4E37V2N1GN5QL, Dura\u00e7\u00e3o: 30 dias"
+        }
+    ]
 }
