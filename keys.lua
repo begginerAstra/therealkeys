6d458398732f1808b81cd74472310f8f6500851c
begginerAstra <euandersonguedes@gmail.com>
2025-06-21 19:22:29 -0300

    Atualizar keys.lua

--- keys.lua
+++ keys.lua
@@ -1,22 +1,178 @@
-return {
-    {
-        Identificador = "Astra",
-        Hwid = "3E90CA7A-D9FE-4725-819D-002521F34CA1",
-        Key = "SUPA-A7X92N3KD"
-    },
-    {
-        Identificador = "Matheus",
-        Hwid = "offC354810B-A7AB-444A-9AFF-169183A9230",
-        Key = "SUPA-AFSAFSAF7SAF75A"
-    },
-    {
-        Identificador = "Demon",
-        Hwid = "D3C7C611-8639-4CBC-BFFD-224489160D03",
-        Key = "SUPA-F0s9MCns952398NSlsafç9NSm"
-    },
-    {
-        Identificador = "Alt",
-        Hwid = "CB9A94F5-E30C-4358-89AB-43D2A99689DA",
-        Key = "SUPA-fm0SncS8kfasnCSCSA8wdfsa"
-    }
+{
+    "keys": [
+        {
+            "key": "SUPA-BPEHGTC5BHC4I6QDF4E37V2N1GN5QL",
+            "created_at": "2025-06-15 15:11:34",
+            "expires_at": "2025-07-15 15:11:34",
+            "used": false,
+            "used_by": null,
+            "used_at": null
+        },
+        {
+            "key": "SUPA-UO62SKBBU6WJDKQ0TL945O5ZDOVYE5",
+            "created_at": "2025-06-15 15:19:26",
+            "expires_at": "2025-06-16 15:19:26",
+            "used": true,
+            "used_by": "244hzkj",
+            "used_at": "2025-06-15 18:53:12",
+            "hwid": "F0035032532-12521512512-125152BS"
+        },
+        {
+            "key": "SUPA-3LKZJJDYNLDA5ZEWEC1BC5PZW8D78I",
+            "created_at": "2025-06-15 15:23:53",
+            "expires_at": "2025-06-17 15:23:53",
+            "used": false,
+            "used_by": null,
+            "used_at": null
+        },
+        {
+            "key": "SUPA-ZG9V3IENQ0J36Q56MHJMTGZABOUUGJ",
+            "created_at": "2025-06-15 16:50:43",
+            "expires_at": "2025-06-17 16:50:43",
+            "used": false,
+            "used_by": null,
+            "used_at": null
+        },
+        {
+            "key": "SUPA-D71IUFBLG0TGNX823UE3XU3V7QLMXF",
+            "created_at": "2025-06-15 18:12:24",
+            "expires_at": "2025-06-17 18:12:24",
+            "used": false,
+            "used_by": null,
+            "used_at": null
+        },
+        {
+            "key": "SUPA-GP4P1GZ5OE66WJ2RZ98CTZGCEZ94AC",
+            "created_at": "2025-06-15 18:16:54",
+            "expires_at": "2025-06-17 18:16:54",
+            "used": false,
+            "used_by": null,
+            "used_at": null
+        },
+        {
+            "key": "SUPA-O95RM15Z4V9Q4FT8L0FCCW73J7QID4",
+            "created_at": "2025-06-15 18:17:26",
+            "expires_at": "2025-06-17 18:17:26",
+            "used": false,
+            "used_by": null,
+            "used_at": null
+        },
+        {
+            "key": "SUPA-E2L9GFDQGVJVCE0YLE2OTC0ZJ5ZVJP",
+            "created_at": "2025-06-15 18:27:54",
+            "expires_at": "2025-06-17 18:27:54",
+            "used": false,
+            "used_by": null,
+            "used_at": null,
+            "hwid": null
+        },
+        {
+            "key": "SUPA-BYEH8HZTGHMLEOBXCCXCK5CEWHSHIW",
+            "created_at": "2025-06-15 18:43:35",
+            "expires_at": "2025-06-16 18:43:35",
+            "used": false,
+            "used_by": null,
+            "used_at": null,
+            "hwid": null
+        },
+        {
+            "key": "SUPA-OQFIC7CS60FWV8MTZPGU8BGXYMTP8W",
+            "created_at": "2025-06-21 22:22:28",
+            "expires_at": "2025-06-22 22:22:28",
+            "used": false,
+            "used_by": null,
+            "used_at": null,
+            "hwid": null
+        }
+    ],
+    "premium_users": [
+        {
+            "user_id": 1269516169673576470,
+            "username": "244hzkj",
+            "activated_at": "2025-06-15 18:53:12",
+            "expires_at": "2025-06-16 15:19:26",
+            "key_used": "SUPA-UO62SKBBU6WJDKQ0TL945O5ZDOVYE5",
+            "hwid": "F0035032532-12521512512-125152BS"
+        }
+    ],
+    "logs": [
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 15:11:37",
+            "details": "Chave: SUPA-BPEHGTC5BHC4I6QDF4E37V2N1GN5QL, Dura\u00e7\u00e3o: 30 dias"
+        },
+        {
+            "action": "SINCRONIZADO_GITHUB",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 15:19:11",
+            "details": "Sincroniza\u00e7\u00e3o manual"
+        },
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 15:19:28",
+            "details": "Chave: SUPA-UO62SKBBU6WJDKQ0TL945O5ZDOVYE5, Dura\u00e7\u00e3o: 1 dias"
+        },
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 15:23:55",
+            "details": "Chave: SUPA-3LKZJJDYNLDA5ZEWEC1BC5PZW8D78I, Dura\u00e7\u00e3o: 2 dias"
+        },
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 16:50:45",
+            "details": "Chave: SUPA-ZG9V3IENQ0J36Q56MHJMTGZABOUUGJ, Dura\u00e7\u00e3o: 2 dias"
+        },
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 18:12:24",
+            "details": "Chave: SUPA-D71IUFBLG0TGNX823UE3XU3V7QLMXF, Dura\u00e7\u00e3o: 2 dias"
+        },
+        {
+            "action": "SINCRONIZADO_GITHUB",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 18:16:21",
+            "details": "Sincroniza\u00e7\u00e3o manual"
+        },
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 18:16:56",
+            "details": "Chave: SUPA-GP4P1GZ5OE66WJ2RZ98CTZGCEZ94AC, Dura\u00e7\u00e3o: 2 dias"
+        },
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 18:17:28",
+            "details": "Chave: SUPA-O95RM15Z4V9Q4FT8L0FCCW73J7QID4, Dura\u00e7\u00e3o: 2 dias"
+        },
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 18:27:56",
+            "details": "Chave: SUPA-E2L9GFDQGVJVCE0YLE2OTC0ZJ5ZVJP, Dura\u00e7\u00e3o: 2 dias"
+        },
+        {
+            "action": "SINCRONIZADO_GITHUB",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 18:28:34",
+            "details": "Sincroniza\u00e7\u00e3o manual"
+        },
+        {
+            "action": "CHAVE_GERADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 18:43:37",
+            "details": "Chave: SUPA-BYEH8HZTGHMLEOBXCCXCK5CEWHSHIW, Dura\u00e7\u00e3o: 1 dias"
+        },
+        {
+            "action": "CHAVE_VALIDADA",
+            "user": "244hzkj",
+            "timestamp": "2025-06-15 18:53:14",
+            "details": "Chave: SUPA-UO62SKBBU6WJDKQ0TL945O5ZDOVYE5, HWID: F0035032532-12521512512-125152BS"
+        }
+    ]
 }
