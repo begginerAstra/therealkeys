1ca8a0e8dd9b5cdb13cc97428cfc15954e1a3d55
begginerAstra <euandersonguedes@gmail.com>
2025-08-16 22:39:03 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -2,7 +2,7 @@ return {
     {
         Identificador = "Astra",
         Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-        Key = "SpecialKey-01"
+        Key = "SUPA-Flu2msm92ityju45ut456tu45mgu5ywejwmt"
     },
     {
         Identificador = "Matheus",
