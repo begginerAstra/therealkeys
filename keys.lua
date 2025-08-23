fdd0c459c9c043e80450e77048f51c19a4736981
begginerAstra <euandersonguedes@gmail.com>
2025-08-23 01:40:47 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -2,7 +2,7 @@ return {
     {
         Identificador = "Astra",
         Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-        Key = "Astra"
+        Key = "Nigger"
     },
     {
         Identificador = "Matheus",
