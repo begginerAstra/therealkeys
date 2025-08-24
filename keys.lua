c4b74aa6cc99d14468304711ee9038a13bd0c3af
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 20:28:42 -0300

    Add key rs_9773E513-67A6-45FE

--- keys.lua
+++ keys.lua
@@ -5,4 +5,10 @@ return {
         Key = "rs_72561B2C-8851-4083",
         Expires = "2025-08-25T23:28:40.462452"
     },
+    {
+        Identificador = "rs",
+        Hwid = "Empty",
+        Key = "rs_9773E513-67A6-45FE",
+        Expires = "2025-08-25T23:28:40.112876"
+    },
 }
