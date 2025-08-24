183894a6a1d54f5173809420596af222ca53ad15
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 01:47:30 -0300

    Add key for Astra

--- keys.lua
+++ keys.lua
@@ -7,6 +7,6 @@ return {
     {
         Identificador = "Astra",
         Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-        Key = "ASTRA-005CC1A9-A702-4D78"
+        Key = "ASTRA-B87BD882-5790-4073"
     },
 }
