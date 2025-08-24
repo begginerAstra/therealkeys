6bd3f886e5bfeac3153d150aafd735d266c4108e
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 02:39:25 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,14 +1,1 @@
-return {
-    {
-        Identificador = "Astra",
-        Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-        Key = "ASTRA-EC27DB05-7D5A-48F4",
-        Expires = "2025-08-25T04:54:10"
-    },
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
-        Key = "Astra_8E082B00-8828-4266",
-        Expires = "2025-08-25T02:00:00"
-    },
-}
+return {}
