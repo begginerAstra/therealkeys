77cc4b84be50a66c410053fc00f3f9004166db1d
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 20:57:15 -0300

    Add key AF2_0A631F65-3166-4665

--- keys.lua
+++ keys.lua
@@ -5,4 +5,10 @@ return {
         Key = "AF2_42A63057-D042-4605",
         Expires = "2025-08-25T23:57:14.012374"
     },
+    {
+        Identificador = "AF2",
+        Hwid = "Empty",
+        Key = "AF2_0A631F65-3166-4665",
+        Expires = "2025-08-25T23:57:13.618692"
+    },
 }
