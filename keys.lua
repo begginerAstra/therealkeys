e06560dbf7dfc50ac73c7bfe9d541caef006ba5b
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 20:57:14 -0300

    Add key AF2_42A63057-D042-4605

--- keys.lua
+++ keys.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+    {
+        Identificador = "AF2",
+        Hwid = "Empty",
+        Key = "AF2_42A63057-D042-4605",
+        Expires = "2025-08-25T23:57:14.012374"
+    },
+}
