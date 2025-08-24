f8f95f5c4128a2fca402c909c9cc87c9dc1eeacb
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 20:56:04 -0300

    Add key Astra_DE2E77C6-ABA2-4DB3

--- keys.lua
+++ keys.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "Astra_DE2E77C6-ABA2-4DB3",
+        Expires = "2025-08-25T23:56:04.084299"
+    },
+}
