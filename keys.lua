4f866b586bd2351632475e4b83f0857563212a90
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 20:05:30 -0300

    Add key Astra

--- keys.lua
+++ keys.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "Astra_2715285B-2E5D-4960",
+        Expires = "2025-08-25T23:05:29.756903"
+    },
+}
