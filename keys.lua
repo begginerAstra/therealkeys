7d1c20e5f3943fb0aa692bb7ab81bc53b2c7f7fd
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 05:39:07 -0300

    Atualizando HWID da key ASTRA.20-7U19-EVZV-TA3Y-GPQE-2WA5

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
     {
             Identificador = "Astra.20",
-            Hwid = "Empty",
+            Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
             Key = "ASTRA.20-7U19-EVZV-TA3Y-GPQE-2WA5",
             Expira = "26/08/2025 08:27"
         }
