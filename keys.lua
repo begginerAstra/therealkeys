24c67daab296ef35b6a7c7daa6adc846683f756c
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 00:20:57 -0300

    Adicionando nova key SHELBY-5AWD-P0XI-XVGT-BKXT-Y34G

--- keys.lua
+++ keys.lua
@@ -10,5 +10,11 @@ return {
         Hwid = "Empty",
         Key = "ASTRA-PU6A-K0O5-IA8L-QPPF-KDUS",
         Expira = "26/08/2025 03:20"
+    },
+    {
+        Identificador = "shelby",
+        Hwid = "Empty",
+        Key = "SHELBY-5AWD-P0XI-XVGT-BKXT-Y34G",
+        Expira = "21/05/2028 03:20"
     }
 }
