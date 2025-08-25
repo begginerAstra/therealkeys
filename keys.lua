e8e45254d3437a41ed96dd7237f51a8c895a83ee
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 20:59:14 -0300

    Adicionando nova key SHELBY-9WC0-JR58-V4X4-HVCD-X1LU

--- keys.lua
+++ keys.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+    {
+            Identificador = "Shelby",
+            Hwid = "Empty",
+            Key = "SHELBY-9WC0-JR58-V4X4-HVCD-X1LU",
+            Expira = "26/08/2025 23:59"
+        }
+}
