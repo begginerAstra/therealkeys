5e1eeea76ba2070411ccac240e8381767cfaa6dc
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 08:52:35 -0300

    Adicionando nova key 4U3ZXI2PGZ41RMXB-ASTRA

--- keys.lua
+++ keys.lua
@@ -1,3 +1,8 @@
 return {
-    
+    {
+            Identificador = "Astra",
+            Hwid = "Empty",
+            Key = "4U3ZXI2PGZ41RMXB-ASTRA",
+            Expira = "25/08/2025 11:54"
+        }
 }
