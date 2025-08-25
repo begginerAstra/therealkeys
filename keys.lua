93beb71bad77709a2ed80120216f2e6323c92d1e
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 08:50:44 -0300

    Limpando keys expiradas

--- keys.lua
+++ keys.lua
@@ -1,8 +1,3 @@
 return {
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
-            Key = "UYBZXK98BLPPOH92-ASTRA",
-            Expira = "25/08/2025 11:47"
-        }
+    
 }
