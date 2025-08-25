9002e752d668f5da7326045250bbf8b42db13fbf
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 04:58:14 -0300

    Adicionando nova key ASTRA-SJ04-VMLS-02GW-FBI9-YX4T

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
         Hwid = "Empty",
         Key = "ASTRA-YWF6-5PC7-A2M2-RPQS-B9VN",
         Expira = "26/08/2025 07:57"
+    },
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "ASTRA-SJ04-VMLS-02GW-FBI9-YX4T",
+        Expira = "26/08/2025 07:58"
     }
 }
