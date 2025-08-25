6a2b480a8ba4d232da9db5b3ecd83b7657e4d113
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 08:45:04 -0300

    Adicionando nova key UYBZXK98BLPPOH92-ASTRA

--- keys.lua
+++ keys.lua
@@ -1,3 +1,8 @@
 return {
-    
+    {
+            Identificador = "Astra",
+            Hwid = "Empty",
+            Key = "UYBZXK98BLPPOH92-ASTRA",
+            Expira = "25/08/2025 11:47"
+        }
 }
