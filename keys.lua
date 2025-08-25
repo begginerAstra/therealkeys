0def21a27aface3bdebba253c71fe7239bc34d77
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 00:33:52 -0300

    Adicionando nova key DRIGOVIADO123AMAPIROCA#FALEITOLEVEIZIPDDYANDYBANIDOPOR10ANOSDOVALORANTSIGMABOYLAVACASATURNOSATURNITA-ZUCO-010C-PEKO-7OZR-PM3D

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
         Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
         Key = "ASTRA-99HX-B1KU-JJZW-F7TS-HOPY",
         Expira = "25/08/2025 15:28"
+    },
+    {
+        Identificador = "drigoviado123amapiroca#faleitoleveizipddyandybanidopor10anosdovalorantsigmaboylavacasaturnosaturnita",
+        Hwid = "Empty",
+        Key = "DRIGOVIADO123AMAPIROCA#FALEITOLEVEIZIPDDYANDYBANIDOPOR10ANOSDOVALORANTSIGMABOYLAVACASATURNOSATURNITA-ZUCO-010C-PEKO-7OZR-PM3D",
+        Expira = "10/01/2053 03:33"
     }
 }
