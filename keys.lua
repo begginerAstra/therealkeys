6a8377ad06e23d56f1a940b45e22679b91c4b37e
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 05:27:40 -0300

    Adicionando nova key ASTRA.20-7U19-EVZV-TA3Y-GPQE-2WA5

--- keys.lua
+++ keys.lua
@@ -1,3 +1,8 @@
 return {
-
+    {
+            Identificador = "Astra.20",
+            Hwid = "Empty",
+            Key = "ASTRA.20-7U19-EVZV-TA3Y-GPQE-2WA5",
+            Expira = "26/08/2025 08:27"
+        }
 }
