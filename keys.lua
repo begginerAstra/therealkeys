bbd3666a797e1c662366bfcd48ec68133fb3f310
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 05:05:24 -0300

    Adicionando nova key ASTRA-WSD0-7DCQ-KJAX-46SJ-RMH4

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
         Hwid = "Empty",
         Key = "ASTRA-A24E-2TP5-8R9Z-I2JJ-I2Z7",
         Expira = "26/08/2025 08:05"
+    },
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "ASTRA-WSD0-7DCQ-KJAX-46SJ-RMH4",
+        Expira = "26/08/2025 08:05"
     }
 }
