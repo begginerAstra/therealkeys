3a1717812624865fdbfeee7ff2ec55c155e6cc4f
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 05:44:05 -0300

    Adicionando nova key TESTE-0B9M-WU0T-IG3U-NZZG-8V8F

--- keys.lua
+++ keys.lua
@@ -1,3 +1,8 @@
 return {
-
+    {
+            Identificador = "Teste",
+            Hwid = "Empty",
+            Key = "TESTE-0B9M-WU0T-IG3U-NZZG-8V8F",
+            Expira = "26/08/2025 08:44"
+        }
 }
