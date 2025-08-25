b280a548294d901ad6fb67d49a518e703a732388
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 08:54:09 -0300

    Removendo keys expiradas

--- keys.lua
+++ keys.lua
@@ -1,50 +1,6 @@
 return {
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
-            Key = "4U3ZXI2PGZ41RMXB-ASTRA",
-            Expira = "25/08/2025 11:54"
-        },
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
-            Key = "36WJZ2PLS5A1A4LA-ASTRA",
-            Expira = "25/08/2025 11:54"
-        },
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
-            Key = "PHTK44EVF3ZPNYXX-ASTRA",
-            Expira = "25/08/2025 11:54"
-        },
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
-            Key = "95BFPPAVG5QFJJKA-ASTRA",
-            Expira = "25/08/2025 11:54"
-        },
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
-            Key = "AE2D8YWHLAGK1AWU-ASTRA",
-            Expira = "25/08/2025 11:54"
-        },
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
-            Key = "6DVSEWCTDWPPMC82-ASTRA",
-            Expira = "25/08/2025 11:54"
-        },
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
-            Key = "ANQDI1F1RZ88JPS7-ASTRA",
-            Expira = "25/08/2025 11:54"
-        },
-    {
-            Identificador = "Astr",
-            Hwid = "Empty",
-            Key = "RCAVRNSHIU3B8D4R-ASTR",
-            Expira = "25/08/2025 11:54"
-        }
+    ,
+    ,
+    ,
+    
 }
