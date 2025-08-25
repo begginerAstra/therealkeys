8c71a4521b6b24bacb871dc0c50b5d79f6581d40
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 20:57:12 -0300

    Adicionando nova key SHELBY-B5B7-4EP4-1CZT-B0VE-L6IZ

--- keys.lua
+++ keys.lua
@@ -1,3 +1,8 @@
 return {
-    
+    {
+            Identificador = "Shelby",
+            Hwid = "Empty",
+            Key = "SHELBY-B5B7-4EP4-1CZT-B0VE-L6IZ",
+            Expira = "26/08/2025 23:57"
+        }
 }
