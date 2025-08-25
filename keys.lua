da2ee69affa982f5d830f116dae315f379d4ed74
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 00:24:54 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,26 +1,3 @@
 return {
-    {
-        Identificador = "Shelby",
-        Hwid = "Empty",
-        Key = "SHELBY-KOOU-POA3-CLXL-B30O-SH33",
-        Expira = "04/09/2025 01:44"
-    },
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
-        Key = "ASTRA-PU6A-K0O5-IA8L-QPPF-KDUS",
-        Expira = "26/08/2025 03:20"
-    },
-    {
-        Identificador = "shelby",
-        Hwid = "Empty",
-        Key = "SHELBY-5AWD-P0XI-XVGT-BKXT-Y34G",
-        Expira = "21/05/2028 03:20"
-    },
-    {
-        Identificador = "shelby",
-        Hwid = "Empty",
-        Key = "SHELBY-PH8P-KTOO-KSVY-Q88G-8HWD",
-        Expira = "10/06/2299 03:23"
-    }
+
 }
