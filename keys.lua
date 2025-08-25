c3d8c6e363c63a451f2d131e754188a19ad0bd90
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 00:20:43 -0300

    Adicionando nova key ASTRA-PU6A-K0O5-IA8L-QPPF-KDUS

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
         Hwid = "Empty",
         Key = "SHELBY-KOOU-POA3-CLXL-B30O-SH33",
         Expira = "04/09/2025 01:44"
+    },
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "ASTRA-PU6A-K0O5-IA8L-QPPF-KDUS",
+        Expira = "26/08/2025 03:20"
     }
 }
