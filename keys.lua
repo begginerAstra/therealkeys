d533508a217422545ca20a55f410f2b23b8e741f
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 22:22:15 -0300

    Adicionando nova key

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
         Hwid = "Empty",
         Key = "ASTRA-NZXW-DJYU-K8EL-GOFI-SCFT",
         Expira = "26/08/2025 01:10"
+    },
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "ASTRA-CYBG-U15X-CJWC-6FQA-MO5V",
+        Expira = "26/08/2025 01:22"
     }
 }
