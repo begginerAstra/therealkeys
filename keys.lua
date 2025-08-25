fc1d3a5e56ca7ea48e55804990680e480e774676
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 22:40:16 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,8 +1,3 @@
 return {
-    {
-        Identificador = "Shelby",
-        Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-        Key = "SHELBY-KSHS-YBZA-RECA-R7RP-2YAE",
-        Expira = "26/08/2025 01:37"
-    }
+
 }
