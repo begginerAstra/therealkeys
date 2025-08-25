18d121fc92dcc2c2a9b6a081689014777e845c3c
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 05:12:04 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,32 +1,3 @@
 return {
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
-        Key = "ASTRA-A24E-2TP5-8R9Z-I2JJ-I2Z7",
-        Expira = "26/08/2025 08:05"
-    },
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
-        Key = "ASTRA-WSD0-7DCQ-KJAX-46SJ-RMH4",
-        Expira = "26/08/2025 08:05"
-    },
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
-        Key = "ASTRA-EGGS-42RO-VRHN-B6Q7-27NT",
-        Expira = "26/08/2025 08:05"
-    },
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
-        Key = "ASTRA-FJK6-JMHC-S614-FMYY-MT0J",
-        Expira = "26/08/2025 08:06"
-    },
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
-        Key = "ASTRA-9NSE-6MFD-315P-J0Q6-LACC",
-        Expira = "26/08/2025 08:07"
-    }
+
 }
