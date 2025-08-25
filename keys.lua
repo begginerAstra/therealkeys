2994a43cd0d5093bd29c2a86738fd044308779a5
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 22:38:54 -0300

    Atualizando HWID da key SHELBY-KSHS-YBZA-RECA-R7RP-2YAE

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
     {
         Identificador = "Shelby",
-        Hwid = "Empty",
+        Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
         Key = "SHELBY-KSHS-YBZA-RECA-R7RP-2YAE",
         Expira = "26/08/2025 01:37"
     }
