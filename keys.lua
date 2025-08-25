c485940c3d99b19a74047f826c741a8a2c3f20a9
begginerAstra <euandersonguedes@gmail.com>
2025-08-24 21:01:04 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,14 +1,2 @@
 return {
-    {
-        Identificador = "AF2",
-        Hwid = "Empty",
-        Key = "AF2_42A63057-D042-4605",
-        Expires = "2025-08-25T23:57:14.012374"
-    },
-    {
-        Identificador = "AF2",
-        Hwid = "Empty",
-        Key = "AF2_2201E5A5-3E41-48DC",
-        Expires = "2025-08-25T23:57:13.587639"
-    },
 }
