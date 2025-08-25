57bfcc976fe8ccc755a0c659fe14781c0afdba9d
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 05:07:17 -0300

    Adicionando nova key ASTRA-9NSE-6MFD-315P-J0Q6-LACC

--- keys.lua
+++ keys.lua
@@ -22,5 +22,11 @@ return {
         Hwid = "Empty",
         Key = "ASTRA-FJK6-JMHC-S614-FMYY-MT0J",
         Expira = "26/08/2025 08:06"
+    },
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "ASTRA-9NSE-6MFD-315P-J0Q6-LACC",
+        Expira = "26/08/2025 08:07"
     }
 }
