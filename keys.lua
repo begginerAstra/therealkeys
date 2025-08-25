9fdc420775c497554fd250d515f6c51843cc5905
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 08:56:58 -0300

    Adicionando nova key ASTRA-6V3X-GW7V-40FO-7701-ST9R

--- keys.lua
+++ keys.lua
@@ -1,4 +1,8 @@
 return {
-    ,
-    
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "ASTRA-6V3X-GW7V-40FO-7701-ST9R",
+        Expira = "26/08/2025 11:56"
+    }
 }
