5b05afee8700fb7f8350cf19cba85e4674362168
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 05:44:24 -0300

    Deletando a key TESTE-0B9M-WU0T-IG3U-NZZG-8V8F

--- keys.lua
+++ keys.lua
@@ -1,8 +1,3 @@
 return {
-    {
-            Identificador = "Teste",
-            Hwid = "Empty",
-            Key = "TESTE-0B9M-WU0T-IG3U-NZZG-8V8F",
-            Expira = "26/08/2025 08:44"
-        }
+    
 }
