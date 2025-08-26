744036a3392527b54ec1d0fb099a5cfc0fc95fd5
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 11:59:57 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,5 +1,5 @@
 return {
-    YCADDDF-2B13-42DA-9867-F259F8566933",
+    "YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "SHELBY-04I2-G37J-9C9O-8U1M-JF5T",
             Expira = 1756306671
         }
