093ab3305eafd426351f345395557f770bdb7836
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 20:51:46 -0300

    Atualizando HWID da key ASTRA-R7ZI-71JG-YN4I-HIVK-1GZF

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "ASTRA-R7ZI-71JG-YN4I-HIVK-1GZF",
             Expira = 1756338549
         }
