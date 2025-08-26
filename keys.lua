ab697bd90bc653ab4c9c7728de2ac096fdd4b710
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 12:16:52 -0300

    Atualizando HWID da key SHELBY-JJVQ-JATP-YCJB-2K3K-3DAL

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "Shelby",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "SHELBY-JJVQ-JATP-YCJB-2K3K-3DAL",
             Expira = 1756307780
         }
