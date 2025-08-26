fd9dea72ff013ebcd1b938f57e3ff62dbeb5998a
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 11:57:25 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,12 +1,1 @@
-return {
-    YCADDDF-2B13-42DA-9867-F259F8566933",
-            Key = "XL-AMY5-O6DN-PTHY-8OMO-1FUZ",
-            Expira = 1756305439
-        },
-    {
-            Identificador = "shelbygoatsupa",
-            Hwid = "Empty",
-            Key = "SHELBYGOATSUPA-JOG8-C2PH-FQXX-4V74-GGTR",
-            Expira = 1756306574
-        }
-}
+return {}
