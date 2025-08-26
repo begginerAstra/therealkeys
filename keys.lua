4acda6bbf7ff8effaa04bb162d6c1011ff1c1a88
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 21:08:46 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,6 +1,8 @@
 return {
-    Shelby = {
+    {
+        Identificador = "Shelby",
         Hwid = "31CADDDF-2B13-42DA-9867-F259F8566933",
         Key = "SHELBY-XOOG-YFF4-89SN-BB06-PK1V",
+        Expira = "27/08/2025 00:00"
     }
 }
