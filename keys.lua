e1fedd890a00449e4ca432bf26e55630c4a575cb
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 20:49:09 -0300

    Adicionando nova key ASTRA-R7ZI-71JG-YN4I-HIVK-1GZF

--- keys.lua
+++ keys.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+    {
+            Identificador = "Astra",
+            Hwid = "Empty",
+            Key = "ASTRA-R7ZI-71JG-YN4I-HIVK-1GZF",
+            Expira = 1756338549
+        }
+}
