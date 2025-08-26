08bf0354aa4a354a41df3c530f6a984357f84554
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 12:03:55 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,28 +1,3 @@
 return {
-    "YCADDDF-2B13-42DA-9867-F259F8566933",
-            Key = "SHELBY-04I2-G37J-9C9O-8U1M-JF5T",
-            Expira = 1756306671
-        },
-    YCADDDF-2B13-42DA-9867-F259F8566933",
-            Key = "ASTRA-9F6G-KBBB-Y14H-7IVH-FLSK",
-            Expira = 1756306807
-        },
-    {
-            Identificador = "Shelcheetos",
-            Hwid = "Empty",
-            Key = "SHELCHEETOS-5WSC-6815-Z5FA-SLNE-L7TE",
-            Expira = 1756306873
-        },
-    {
-            Identificador = "Oi",
-            Hwid = "Empty",
-            Key = "OI-HQEN-3EZB-ES86-4MD6-E5T2",
-            Expira = 1756306890
-        },
-    {
-            Identificador = "5WAS",
-            Hwid = "Empty",
-            Key = "5WAS-L8RY-LA5N-CKVS-B26L-2DFG",
-            Expira = 1756307001
-        }
+
 }
