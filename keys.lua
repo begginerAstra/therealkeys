045403c89e1295b201effb02cbef38c2d769773b
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:20:04 -0300

    Adicionando nova key PINGO-OALO-Q5OZ-HS0G-ETG3-H9E6

--- keys.lua
+++ keys.lua
@@ -1,2 +1,8 @@
 return {
+    {
+            Identificador = "Pingo",
+            Hwid = "Empty",
+            Key = "PINGO-OALO-Q5OZ-HS0G-ETG3-H9E6",
+            Expira = 1842632403
+        }
 }
