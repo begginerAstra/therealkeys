850ed53c751fe0147d5b802dab4f4debfc4ec561
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 11:57:51 -0300

    Adicionando nova key SHELBY-04I2-G37J-9C9O-8U1M-JF5T

--- keys.lua
+++ keys.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+    {
+            Identificador = "Shelby",
+            Hwid = "Empty",
+            Key = "SHELBY-04I2-G37J-9C9O-8U1M-JF5T",
+            Expira = 1756306671
+        }
+}
