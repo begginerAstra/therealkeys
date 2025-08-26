3ed0923e9a2e8507f8869035d87c9e1b5f87ae7d
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:23:40 -0300

    Adicionando nova key ASTRA-XYZM-88BF-YNC6-DICA-M19J

--- keys.lua
+++ keys.lua
@@ -1,3 +1,8 @@
 return {
-    
+    {
+            Identificador = "astra",
+            Hwid = "Empty",
+            Key = "ASTRA-XYZM-88BF-YNC6-DICA-M19J",
+            Expira = 1756319020
+        }
 }
