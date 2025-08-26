74c555ad8258fa051b737918f4a4358ab8c11958
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 12:16:21 -0300

    Adicionando nova key SHELBY-JJVQ-JATP-YCJB-2K3K-3DAL

--- keys.lua
+++ keys.lua
@@ -1,2 +1,8 @@
 return {
+    {
+            Identificador = "Shelby",
+            Hwid = "Empty",
+            Key = "SHELBY-JJVQ-JATP-YCJB-2K3K-3DAL",
+            Expira = 1756307780
+        }
 }
