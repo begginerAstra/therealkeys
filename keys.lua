966e01d7f90e7b4a05bc413d3bf67af4bebb603d
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 20:38:36 -0300

    Adicionando nova key LIMOS-VEZB-M36F-0FTS-QDJW-VXKI

--- keys.lua
+++ keys.lua
@@ -1,2 +1,8 @@
 return {
+    {
+            Identificador = "Limos",
+            Hwid = "Empty",
+            Key = "LIMOS-VEZB-M36F-0FTS-QDJW-VXKI",
+            Expira = 1756337916
+        }
 }
