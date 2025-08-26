d1201e38bdae5b39b181f85eac00487a68bcaf5e
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 21:04:57 -0300

    Adicionando nova key SHE-5VDR-BHN4-FVJA-2RZ9-AOSJ

--- keys.lua
+++ keys.lua
@@ -1,5 +1,8 @@
 return {
-    Identificador = "Shelby",
-    Hwid = "31CADDDF-2B13-42DA-9867-F259F8566933",
-    Key = "SHELBY-XOOG-YFF4-89SN-BB06-PK1V",
+    {
+            Identificador = "she",
+            Hwid = "Empty",
+            Key = "SHE-5VDR-BHN4-FVJA-2RZ9-AOSJ",
+            Expira = "27/08/2025 00:04"
+        }
 }
