d6edba5be1f4c176e830c50aa1d7579edfe5682d
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 20:46:25 -0300

    Atualizando HWID da key ASTRA-4CA3-DY0Y-SHOW-8QHB-9BJN

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "ASTRA-4CA3-DY0Y-SHOW-8QHB-9BJN",
             Expira = 1756252072
         }
