57ce5c41659187204bff86b01f369104be965798
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 12:09:50 -0300

    Adicionando nova key SHELBY-SVTP-GA06-1397-Q2XF-ZGFQ

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
             Hwid = "Empty",
             Key = "SHELBY-F03M-OV22-4XU1-A3JK-PKRG",
             Expira = 1756307368
+        },
+    {
+            Identificador = "Shelby",
+            Hwid = "Empty",
+            Key = "SHELBY-SVTP-GA06-1397-Q2XF-ZGFQ",
+            Expira = 1756307390
         }
 }
