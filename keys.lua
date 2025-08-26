16e538752bfdd4c22213840ab7a591d3b5095695
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:20:16 -0300

    Deletando a key PINGO-OALO-Q5OZ-HS0G-ETG3-H9E6

--- keys.lua
+++ keys.lua
@@ -1,8 +1,3 @@
 return {
-    {
-            Identificador = "Pingo",
-            Hwid = "Empty",
-            Key = "PINGO-OALO-Q5OZ-HS0G-ETG3-H9E6",
-            Expira = 1842632403
-        }
+    
 }
