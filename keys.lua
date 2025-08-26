83d13ce3ca51ac5756b28e3ec81fdd5c7fc3cedb
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 11:56:14 -0300

    Adicionando nova key SHELBYGOATSUPA-JOG8-C2PH-FQXX-4V74-GGTR

--- keys.lua
+++ keys.lua
@@ -22,5 +22,11 @@ return {
             Hwid = "Empty",
             Key = "XL-AMY5-O6DN-PTHY-8OMO-1FUZ",
             Expira = 1756305439
+        },
+    {
+            Identificador = "shelbygoatsupa",
+            Hwid = "Empty",
+            Key = "SHELBYGOATSUPA-JOG8-C2PH-FQXX-4V74-GGTR",
+            Expira = 1756306574
         }
 }
