3cd93b9b4ef0a94bd1a634c69cef46189c3996c0
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 12:04:58 -0300

    Atualizando HWID da key 5WAS-R82F-PV81-5KHM-X6FO-I4SM

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "5WAS",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "5WAS-R82F-PV81-5KHM-X6FO-I4SM",
             Expira = 1756307056
         }
