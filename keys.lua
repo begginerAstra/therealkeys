b5363798e318af54b0edfe21745a4a6a20ebcc5d
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 12:00:08 -0300

    Adicionando nova key ASTRA-9F6G-KBBB-Y14H-7IVH-FLSK

--- keys.lua
+++ keys.lua
@@ -2,5 +2,11 @@ return {
     "YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "SHELBY-04I2-G37J-9C9O-8U1M-JF5T",
             Expira = 1756306671
+        },
+    {
+            Identificador = "Astra",
+            Hwid = "Empty",
+            Key = "ASTRA-9F6G-KBBB-Y14H-7IVH-FLSK",
+            Expira = 1756306807
         }
 }
