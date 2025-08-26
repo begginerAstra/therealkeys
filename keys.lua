3c3f72c8630b67afd697df1f6f275a6653537532
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 21:00:02 -0300

    Adicionando nova key SHELBY-XOOG-YFF4-89SN-BB06-PK1V

--- keys.lua
+++ keys.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+    {
+            Identificador = "Shelby",
+            Hwid = "Empty",
+            Key = "SHELBY-XOOG-YFF4-89SN-BB06-PK1V",
+            Expira = "27/08/2025 00:00"
+        }
+}
