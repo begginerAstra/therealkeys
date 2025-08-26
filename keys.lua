6b852a1aad48a5cfd333a3fae3efad0d3276dc95
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:23:52 -0300

    Deletando a key ASTRA-XYZM-88BF-YNC6-DICA-M19J

--- keys.lua
+++ keys.lua
@@ -1,8 +1,3 @@
 return {
-    {
-            Identificador = "astra",
-            Hwid = "Empty",
-            Key = "ASTRA-XYZM-88BF-YNC6-DICA-M19J",
-            Expira = 1756319020
-        }
+    
 }
