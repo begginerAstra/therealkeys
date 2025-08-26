46473511e792af5b1f1d4ac60f6f99be4b395dd0
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:14:48 -0300

    Adicionando nova key PINGO-XEUR-U857-WILQ-XN2R-U04Q

--- keys.lua
+++ keys.lua
@@ -1,3 +1,8 @@
 return {
-    
+    {
+            Identificador = "Pingo",
+            Hwid = "Empty",
+            Key = "PINGO-XEUR-U857-WILQ-XN2R-U04Q",
+            Expira = 1842632088
+        }
 }
