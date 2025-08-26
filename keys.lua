3dd4822808a6d7380263461f232fdbfd02670112
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 14:55:58 -0300

    Deletando a key KEVYN-LVTU-DYCR-EX3F-XO4Z-O5L5

--- keys.lua
+++ keys.lua
@@ -1,14 +1,3 @@
 return {
-    {
-            Identificador = "Shelby",
-            Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-            Key = "SHELBY-3HPC-0IVH-FY2W-5JE5-IFHE",
-            Expira = 1756311139
-        },
-    {
-            Identificador = "Kevyn",
-            Hwid = "Empty",
-            Key = "KEVYN-LVTU-DYCR-EX3F-XO4Z-O5L5",
-            Expira = 1758822775
-        }
+    
 }
