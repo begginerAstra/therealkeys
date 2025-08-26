5b6f80e30c868a2da256d52aa6bf2ac9ee0fbf08
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 11:59:37 -0300

    Atualizando HWID da key SHELBY-04I2-G37J-9C9O-8U1M-JF5T

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "Shelby",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "SHELBY-04I2-G37J-9C9O-8U1M-JF5T",
             Expira = 1756306671
         }
