f9bd55a9e2a85f542702f7afb6c1ba0255b2714c
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:24:33 -0300

    Adicionando nova key ASTRA-V2KV-SUOL-9JMO-VZVP-4NR2

--- keys.lua
+++ keys.lua
@@ -1,2 +1,8 @@
 return {
+    {
+            Identificador = "astra",
+            Hwid = "Empty",
+            Key = "ASTRA-V2KV-SUOL-9JMO-VZVP-4NR2",
+            Expira = 1756319072
+        }
 }
