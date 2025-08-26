5e3b786957c15b32898d4bfcb62e1d9a441c5885
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 20:48:15 -0300

    Atualizando HWID da key ASTRA-07T5-8HZM-7PNR-JPR2-GE3R

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "ASTRA-07T5-8HZM-7PNR-JPR2-GE3R",
             Expira = 1756252141
         }
