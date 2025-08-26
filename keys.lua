abea71180c4a4a371a2a2eeef1b7c4b054946063
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 20:41:12 -0300

    Atualizando HWID da key LIMOS-VEZB-M36F-0FTS-QDJW-VXKI

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "Limos",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "LIMOS-VEZB-M36F-0FTS-QDJW-VXKI",
             Expira = 1756337916
         }
