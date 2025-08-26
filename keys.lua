2887d1bde061d113f7ede0923ef6f1a092ffd004
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:15:41 -0300

    Atualizando HWID da key PINGO-XEUR-U857-WILQ-XN2R-U04Q

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "Pingo",
-            Hwid = "Empty",
+    J345-12345-12345-12345-12345",
             Key = "PINGO-XEUR-U857-WILQ-XN2R-U04Q",
             Expira = 1842632088
         }
