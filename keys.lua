90018013814d5d0d64a66dee9b6730b9c4b20390
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 21:06:29 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,6 +1,6 @@
 return {
-    YCADDDF-2B13-42DA-9867-F259F8566933",
-            Key = "SHE-5VDR-BHN4-FVJA-2RZ9-AOSJ",
-            Expira = "27/08/2025 00:04"
-        }
+    Shelby = {
+        Hwid = "31CADDDF-2B13-42DA-9867-F259F8566933",
+        Key = "SHELBY-XOOG-YFF4-89SN-BB06-PK1V",
+    }
 }
