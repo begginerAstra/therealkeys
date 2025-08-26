51fe226328b6cc902e726d4a1e9640e2b28d21e2
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 13:11:19 -0300

    Atualizando HWID da key ASRA-W56S-K2ZZ-N8SC-6UF9-H96E

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
     {
             Identificador = "Asra",
-            Hwid = "Empty",
+            Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
             Key = "ASRA-W56S-K2ZZ-N8SC-6UF9-H96E",
             Expira = 1756310985
         }
