e0f75ca74eca69fbe44d66a1e0aeec94ac1f2d9e
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 12:08:05 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,6 +1,5 @@
 return {
-    YCADDDF-2B13-42DA-9867-F259F8566933",
-            Key = "5WAS-R82F-PV81-5KHM-X6FO-I4SM",
-            Expira = 1756307056
-        }
+    Identificador = "YCADDDF-2B13-42DA-9867-F259F8566933",
+    Key = "5WAS-R82F-PV81-5KHM-X6FO-I4SM",
+    Expira = 1756307056
 }
