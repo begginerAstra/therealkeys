9779ec3609e80970e60f8dea54c988ca26e74ae3
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 20:45:53 -0300

    Adicionando nova key ASTRA-4CA3-DY0Y-SHOW-8QHB-9BJN

--- keys.lua
+++ keys.lua
@@ -1,2 +1,8 @@
 return {
+    {
+            Identificador = "Astra",
+            Hwid = "Empty",
+            Key = "ASTRA-4CA3-DY0Y-SHOW-8QHB-9BJN",
+            Expira = 1756252072
+        }
 }
