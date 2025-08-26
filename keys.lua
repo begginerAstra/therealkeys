b87d5b956a3ece0069fc63cbc914f2f4cb8f3ed3
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:24:34 -0300

    Adicionando nova key ASTR-2BUD-0TSA-X53T-3857-JVF8

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
             Hwid = "Empty",
             Key = "ASTRA-V2KV-SUOL-9JMO-VZVP-4NR2",
             Expira = 1756319072
+        },
+    {
+            Identificador = "astr",
+            Hwid = "Empty",
+            Key = "ASTR-2BUD-0TSA-X53T-3857-JVF8",
+            Expira = 1756319073
         }
 }
