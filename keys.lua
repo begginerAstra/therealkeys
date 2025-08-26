edba37b76218f292a644c259f0480abca40b2e41
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 12:15:43 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,12 +1,1 @@
-return {
-    YCADDDF-2B13-42DA-9867-F259F8566933",
-            Key = "SHELBY-F03M-OV22-4XU1-A3JK-PKRG",
-            Expira = 1756307368
-        },
-    {
-            Identificador = "Shelby",
-            Hwid = "Empty",
-            Key = "SHELBY-SVTP-GA06-1397-Q2XF-ZGFQ",
-            Expira = 1756307390
-        }
-}
+return {}
