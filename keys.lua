bc35afe3ec2ca24221f16d5e863b99a6a69925a7
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 20:47:02 -0300

    Adicionando nova key ASTRA-07T5-8HZM-7PNR-JPR2-GE3R

--- keys.lua
+++ keys.lua
@@ -1,3 +1,8 @@
 return {
-
+    {
+            Identificador = "Astra",
+            Hwid = "Empty",
+            Key = "ASTRA-07T5-8HZM-7PNR-JPR2-GE3R",
+            Expira = 1756252141
+        }
 }
