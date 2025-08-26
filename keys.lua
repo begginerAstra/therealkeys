4c5e98684790f5d64843140202230477747a6664
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 13:13:52 -0300

    Deletando a key ASRA-W56S-K2ZZ-N8SC-6UF9-H96E

--- keys.lua
+++ keys.lua
@@ -1,11 +1,5 @@
 return {
     {
-            Identificador = "Asra",
-            Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-            Key = "ASRA-W56S-K2ZZ-N8SC-6UF9-H96E",
-            Expira = 1756310985
-        },
-    {
             Identificador = "Shelby",
             Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
             Key = "SHELBY-3HPC-0IVH-FY2W-5JE5-IFHE",
