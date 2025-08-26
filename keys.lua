07335a2c9b62f88955e7d37543afc3c7d1c4c52c
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 13:12:20 -0300

    Adicionando nova key SHELBY-3HPC-0IVH-FY2W-5JE5-IFHE

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
             Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
             Key = "ASRA-W56S-K2ZZ-N8SC-6UF9-H96E",
             Expira = 1756310985
+        },
+    {
+            Identificador = "Shelby",
+            Hwid = "Empty",
+            Key = "SHELBY-3HPC-0IVH-FY2W-5JE5-IFHE",
+            Expira = 1756311139
         }
 }
