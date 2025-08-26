c35d8af8bfbe321a691326387a652c4da8ea43d8
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 11:56:31 -0300

    Atualizando HWID da key SHELBYGOATSUPA-JOG8-C2PH-FQXX-4V74-GGTR

--- keys.lua
+++ keys.lua
@@ -1,25 +1,5 @@
 return {
-    {
-        Identificador = "Shelby",
-        Hwid = "31CADDDF-2B13-42DA-9867-F259F8566933",
-        Key = "SHELBY-XOOG-YFF4-89SN-BB06-PK1V",
-        Expira = "27/08/2025 00:00"
-    },
-    {
-        Identificador = "Actually",
-        Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-        Key = "ACTUALLY-P5OJ-X2XV-WFXR-MEIS-SNS4",
-        Expira = "28/08/2025 00:00"
-    },
-    {
-            Identificador = "Astra",
-            Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-            Key = "ASTRA-BXAR-B0PC-94D3-8A7X-ZTM1",
-            Expira = 1756293183
-        },
-    {
-            Identificador = "XL",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "XL-AMY5-O6DN-PTHY-8OMO-1FUZ",
             Expira = 1756305439
         },
