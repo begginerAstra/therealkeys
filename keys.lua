bafeccbe08236cc113aa40b54be62f2324720ce9
begginerAstra <euandersonguedes@gmail.com>
2025-08-26 15:24:41 -0300

    Adicionando nova key ASTR-3F5D-6DDF-VOVZ-8MQK-HHRR

--- keys.lua
+++ keys.lua
@@ -10,5 +10,11 @@ return {
             Hwid = "Empty",
             Key = "ASTR-2BUD-0TSA-X53T-3857-JVF8",
             Expira = 1756319073
+        },
+    {
+            Identificador = "astr",
+            Hwid = "Empty",
+            Key = "ASTR-3F5D-6DDF-VOVZ-8MQK-HHRR",
+            Expira = 1756319081
         }
 }
