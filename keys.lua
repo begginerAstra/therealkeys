8005aafc40a585026563d9b25f95ef86a9e90068
begginerAstra <euandersonguedes@gmail.com>
2025-08-25 21:05:18 -0300

    Atualizando HWID da key SHE-5VDR-BHN4-FVJA-2RZ9-AOSJ

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-            Identificador = "she",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "SHE-5VDR-BHN4-FVJA-2RZ9-AOSJ",
             Expira = "27/08/2025 00:04"
         }
