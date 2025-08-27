ddd9dc938c3aefd0afd6590abeb153ffc696230d
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 00:29:55 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,10 +1,2 @@
 return {
-    YCADDDF-2B13-42DA-9867-F259F8566933",
-            Key = "DRIGOGAY-HE03-7B10-8PER-L1KM-D6X6",
-            Expira = 1756351260
-        },
-    YCADDDF-2B13-42DA-9867-F259F8566933",
-        Key = "ASTRA-YLM8-MJK3-20VP-9QXE-KMDH",
-        Expira = 1756351735
-    }
 }
