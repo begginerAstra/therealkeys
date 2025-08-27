f6046caadfa72efd94a57378f9b4b7708f529366
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 00:01:28 -0300

    Atualizando HWID da key ASTRA-8DJ5-UE53-YQ9K-PA80-PAQB

--- keys.lua
+++ keys.lua
@@ -3,9 +3,7 @@ return {
             Key = "ASTRA-R7ZI-71JG-YN4I-HIVK-1GZF",
             Expira = 1756338549
         },
-    {
-            Identificador = "Astra",
-            Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
             Key = "ASTRA-8DJ5-UE53-YQ9K-PA80-PAQB",
             Expira = 1756350024
         }
