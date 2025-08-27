9858179327dcd6e7ae844cf2a71027c9b1574ea5
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 01:01:10 -0300

    Atualizando HWID da key ASTRA-OK9Y-1CGV-DCZ2-1BJP-Y4P9

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
         Key = "ASTRA-DN5F-9PHA-QLMP-3XX0-ZKH9",
         Expira = 1756353403
     },
