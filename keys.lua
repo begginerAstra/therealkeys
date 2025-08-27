1f6b866b7c2561852958a366cb2a62adf13268aa
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 01:11:41 -0300

    Adicionando nova key ASTRA-1Q2V-R7J3-JJQY-2DEH-XECA

--- keys.lua
+++ keys.lua
@@ -2,5 +2,11 @@ return {,
     YCADDDF-2B13-42DA-9867-F259F8566933",
         Key = "ASTRA-VC3E-OW56-MOHS-TAQF-EXPH",
         Expira = 1756354215
+    },
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "ASTRA-1Q2V-R7J3-JJQY-2DEH-XECA",
+        Expira = 1756354300
     }
 }
