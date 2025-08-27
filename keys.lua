03a09a9d8efb0a4a29feadded4519ca4ac4bf936
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 01:01:25 -0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,12 +1,2 @@
 return {
-    YCADDDF-2B13-42DA-9867-F259F8566933",
-        Key = "ASTRA-DN5F-9PHA-QLMP-3XX0-ZKH9",
-        Expira = 1756353403
-    },
-    {
-    Identificador = "Astra",
-    Hwid = "Empty",
-    Key = "ASTRA-OK9Y-1CGV-DCZ2-1BJP-Y4P9",
-    Expira = 1756353647
-}
 }
