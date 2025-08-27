2dfaa6a665fd4d1ab1a1ee3a1d4000a85660836a
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 00:21:00 -0300

    Adicionando nova key DRIGOGAY-HE03-7B10-8PER-L1KM-D6X6

--- keys.lua
+++ keys.lua
@@ -1,2 +1,8 @@
 return {
+    {
+            Identificador = "drigogay",
+            Hwid = "Empty",
+            Key = "DRIGOGAY-HE03-7B10-8PER-L1KM-D6X6",
+            Expira = 1756351260
+        }
 }
