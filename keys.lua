9b1631d0f282d06199ddfce437d2895ac9944468
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 00:29:38 -0300

    Atualizando HWID da key ASTRA-YLM8-MJK3-20VP-9QXE-KMDH

--- keys.lua
+++ keys.lua
@@ -3,9 +3,7 @@ return {
             Key = "DRIGOGAY-HE03-7B10-8PER-L1KM-D6X6",
             Expira = 1756351260
         },
-    {
-        Identificador = "Astra",
-        Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
         Key = "ASTRA-YLM8-MJK3-20VP-9QXE-KMDH",
         Expira = 1756351735
     }
