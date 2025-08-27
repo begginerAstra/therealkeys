302a5429f7d15530deb7c9ced14883dadd3d2205
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 01:01:47 -0300

    Atualizando HWID da key ASTRA-JCTV-9MAJ-WP2V-SC2A-TAWT

--- keys.lua
+++ keys.lua
@@ -1,7 +1,5 @@
 return {
-    {
-    Identificador = "astra",
-    Hwid = "Empty",
+    YCADDDF-2B13-42DA-9867-F259F8566933",
     Key = "ASTRA-JCTV-9MAJ-WP2V-SC2A-TAWT",
     Expira = 1756353691
 }
