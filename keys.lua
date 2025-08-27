da31345a8f38dd22718c6637252e099e155d9421
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 01:00:48 -0300

    Adicionando nova key ASTRA-OK9Y-1CGV-DCZ2-1BJP-Y4P9

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
         Hwid = "Empty",
         Key = "ASTRA-DN5F-9PHA-QLMP-3XX0-ZKH9",
         Expira = 1756353403
-    }
+    },
+    {
+    Identificador = "Astra",
+    Hwid = "Empty",
+    Key = "ASTRA-OK9Y-1CGV-DCZ2-1BJP-Y4P9",
+    Expira = 1756353647
+}
 }
