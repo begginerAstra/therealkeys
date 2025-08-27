45a7f726a22381a6616ddab56a472ea990c98b48
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 00:28:56 -0300

    Adicionando nova key ASTRA-YLM8-MJK3-20VP-9QXE-KMDH

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
             Hwid = "Empty",
             Key = "DRIGOGAY-HE03-7B10-8PER-L1KM-D6X6",
             Expira = 1756351260
-        }
+        },
+    {
+        Identificador = "Astra",
+        Hwid = "Empty",
+        Key = "ASTRA-YLM8-MJK3-20VP-9QXE-KMDH",
+        Expira = 1756351735
+    }
 }
