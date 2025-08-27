e345fe2e3f39d7148a2ae342390344420856ea74
begginerAstra <euandersonguedes@gmail.com>
2025-08-27 00:19:37 -0300

    Adicionando nova key DRIGO-MY0S-5UDV-XBJH-E73N-0D9E

--- keys.lua
+++ keys.lua
@@ -5,5 +5,11 @@ return {
         Hwid = "Empty",
         Key = "SHELBY-MX1G-Q7G8-CF82-1G0B-4CTU-3QWM",
         Expira = 1756350466
-    }
+    },
+    {
+            Identificador = "drigo",
+            Hwid = "Empty",
+            Key = "DRIGO-MY0S-5UDV-XBJH-E73N-0D9E",
+            Expira = 1756351176
+        }
 }
