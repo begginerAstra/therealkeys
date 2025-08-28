108631d677ef881e01bbe318d2f2c9601c3d8247
begginerAstra <euandersonguedes@gmail.com>
2025-08-28 20:13:23 -0300

    Registrando HWID 31CADDDF-2B13-42DA-9867-F259F8566933

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
   {
     Identificador = "Astra",
-    Hwid = "Empty",
+    Hwid = "31CADDDF-2B13-42DA-9867-F259F8566933",
     Key = "SUPA-53YR-TMR5-EM0T",
     Expira = 1756509184,
   },
