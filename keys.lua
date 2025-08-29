596f5c8bd2731691169198ae7aee827f9395e5a8
begginerAstra <euandersonguedes@gmail.com>
2025-08-29 10:17:25 -0300

    Adicionando key SUPA-36WQREZ0RZ-DWXMGHEE53-7HKQQ2Z3RX

--- keys.lua
+++ keys.lua
@@ -5,4 +5,10 @@ return {
     Key = "SUPA-MY9SV43568-WJQXK0YY62-2XXFK8CKKB",
     Expira = 88156386914,
   },
+  {
+    Identificador = "Pingo",
+    Hwid = "Empty",
+    Key = "SUPA-36WQREZ0RZ-DWXMGHEE53-7HKQQ2Z3RX",
+    Expira = 88156387044,
+  },
 }
