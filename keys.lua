0031c9b4f5fc996b22c195a5d930224347ef6cce
begginerAstra <euandersonguedes@gmail.com>
2025-08-29 10:04:12 -0300

    Deletando key SUPA-I266VJZVXR-QCHZPDUSJ3-NC7GGQ0GD4

--- keys.lua
+++ keys.lua
@@ -6,12 +6,6 @@ return {
     Expira = 1756509184,
   },
   {
-    Identificador = "Astra",
-    Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-    Key = "SUPA-I266VJZVXR-QCHZPDUSJ3-NC7GGQ0GD4",
-    Expira = 1756509310,
-  },
-  {
     Identificador = "shelby",
     Hwid = "31CADDDF-2B13-42DA-9867-F259F8566933",
     Key = "SUPA-BXDJA15042-G7KQTA2KVT-ABM3NTDATY",
