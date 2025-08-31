29e44c88b5cc30dd1fc41a8bc1e366cfa018e7f7
begginerAstra <euandersonguedes@gmail.com>
2025-08-31 02:12:32 -0300

    Registrando HWID B8576E3F-1BA0-451F-AD02-93DC41B29743

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
   {
     Identificador = "Astra",
-    Hwid = "Empty",
+    Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
     Key = "SUPA-770XM7V6K0-O4VV0VVYU8-A7SOZDXLFO",
     Expira = 2620530707,
   },
