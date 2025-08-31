43a887ba23835788e82e670368ea218529fd4e38
begginerAstra <euandersonguedes@gmail.com>
2025-08-31 01:52:10 -0300

    Registrando HWID 31CADDDF-2B13-42DA-9867-F259F8566933

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
   {
     Identificador = "XL",
-    Hwid = "Empty",
+    Hwid = "31CADDDF-2B13-42DA-9867-F259F8566933",
     Key = "SUPA-HWCCZH257J-WA7VGF7I0W-VJZSDUNTLG",
     Expira = 10396529504,
   },
